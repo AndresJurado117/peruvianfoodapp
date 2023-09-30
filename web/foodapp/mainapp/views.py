3c2ba6631e016cd56d113d76a4772ceaf49788d8
XJuanRocksX <89615507+XJuanRocksX@users.noreply.github.com>
2023-09-29 21:32:28 -0500

    Updated CSS

--- web/foodapp/mainapp/views.py
+++ web/foodapp/mainapp/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.urls import reverse
-from unidecode import unidecode
 import requests
 
 API_HOST = "http://127.0.0.1:80"
@@ -13,17 +12,16 @@ async def index(request):
     response = requests.get(f"{API_HOST}/random_recipe", headers=headers).json()
     return render(request, "mainapp/recipe.html", {"response": response})
 
-async def recipe(request, recipe_id):
+async def recipe(request, recipe_id: int) -> render:
     headers = {"API-Key": API_KEY}
     response = requests.get(f"{API_HOST}/recipes/{recipe_id}", headers=headers).json()
     return render(request, "mainapp/recipe.html", {"response" : response})
 
-async def search(request):
+async def search(request) -> render:
     headers = {"API-Key": API_KEY}
     query = request.GET["query"]
     if query:
         response = requests.get(f"{API_HOST}/search_recipe/{query}", headers=headers).json()
-        print(response, len(response), type(response), type(response) == dict)
         if type(response) == dict and len(response) > 1:
             return HttpResponseRedirect(reverse("recipe", kwargs={"recipe_id": response["id"]}))
         elif "detail" in response:
